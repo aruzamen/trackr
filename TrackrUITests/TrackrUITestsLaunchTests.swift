2b389e37075902c46f064bc89639846adcdb053e
Marcelo Aruzamen <aruzamen@gmail.com>
2024-03-21 01:17:41 -0400

    Initial Commit

---
+++ TrackrUITests/TrackrUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TrackrUITestsLaunchTests.swift
+//  TrackrUITests
+//
+//  Created by Marcelo Aruzamen on 21/3/24.
+//
+
+import XCTest
+
+final class TrackrUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
